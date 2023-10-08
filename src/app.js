e81e95d66a2b2251a30b0ce08bc6689c49517c30
arat-arjat <124855089+arat-arjat@users.noreply.github.com>
2023-10-08 21:32:58 +0000

    culminado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["El carpintero", "Mi socio", "mi jefe", "mi esposa"];
+  let action = ["no entrego", "se enfermo", "hizo mal", "olvido"];
+  let what = ["el auto", "mi trabajo", "la medida del objeto"];
+  let when = [
+    "esta mañana",
+    "al salir del taller",
+    "en la casa",
+    "en el estacionamiento",
+    "mientras iba de camino"
+  ];
+
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let que = what[Math.floor(Math.random() * what.length)];
+  let donde = when[Math.floor(Math.random() * when.length)];
+
+  //let suma2 = quien + " " + accion + " " + que + " " + donde;
+
+  let suma = `${quien} ${accion} ${que}  ${donde} `;
+
+  let elemento = document.getElementById("excusasFalsas");
+  elemento.innerText = suma;
 };
